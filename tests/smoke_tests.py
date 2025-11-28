2f97b4249bffab225ddc1c7b3570a497fb771d85
rai-is-learning <githubstudent@ricafrente.com>
2025-11-27 22:36:28 -0800

    Update smoke_tests.py

--- tests/smoke_tests.py
+++ tests/smoke_tests.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "https://finapi-yourname.azurewebsites.net" # <------ Replace with your actual base URL
+BASE_URL = "https://finapi-rai.azurewebsites.net" # <------ Replace with your actual base URL
 
 def test_home():
     r = requests.get(f"{BASE_URL}/")
